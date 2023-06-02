b6db5e67ccd910fe7c52fb41125e420adf0c6451
h-ris <95009925+h-ris@users.noreply.github.com>
2023-06-02 12:17:34 -0400

    Update CircularSawBladeVerifier.java

--- CircularSawBladeVerifier.java
+++ CircularSawBladeVerifier.java
@@ -1,7 +1,4 @@
 /*
- * Student Name: Huixin Xu
- * Lab Professor: David Houtman
- * Due Date: Nov 25th, 2022
  * Date modified: Nov 14th, 2022
  * Description: This class is used to indicate if the circular saw blade
  *              diameter and kerf is within tolerance.
