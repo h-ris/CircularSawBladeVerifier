929720e5cb27de1bed9dfe17c77fc4066bfc73f8
h-ris <95009925+h-ris@users.noreply.github.com>
2023-06-02 12:17:02 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,7 +1,4 @@
 /*
- * Student Name: Huixin Xu
- * Lab Professor: David Houtman
- * Due Date: Nov 25th, 2022
  * Date modified: Nov 14th, 2022
  * Description: This class contains method inputPositiveDouble(String prompt) ensures that only 
  *   positive numbers are entered, otherwise it will prompt invalid input.
